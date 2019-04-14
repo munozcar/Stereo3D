990c995cd0b617cb38cf0c5120f765bcbb005dd8
Carlos <munozcar@grinnell.edu>
2019-04-13 23:23:38 -0500

    automatic constructions

--- epipolar.m
+++ epipolar.m
@@ -2,8 +2,19 @@
 % Getting Correspondences
 
 % Load stereo images 
-img_left = im2double(rgb2gray(imread('/home/weinman/courses/CSC262/images/left.jpg')));
-img_right = im2double(rgb2gray(imread('/home/weinman/courses/CSC262/images/right.jpg'))); 
+img_left = im2double(rgb2gray(imread('/home/munozcar/Desktop/left.jpg')));
+img_right = im2double(rgb2gray(imread('/home/munozcar/Desktop/right.jpg'))); 
+
+figure(1);
+imshow(img_left);
+title('Left Image');
+pause(0.5);
+
+figure(2);
+imshow(img_right);
+title('Right Image');
+pause(0.5);
+
 
 features_left = kpdet(img_left);
 features_right = kpdet(img_right);
@@ -15,6 +26,9 @@ descriptors_right = kpfeat(img_right, features_right);
 [rows2, cols2] = find(features_right);
 
 feat_matrix = zeros(size(cols1, 1), 2);
+xx = zeros(size(cols1, 1), 2);
+yy = zeros(size(cols1, 1), 2);
+
 
 % Calculating translations for each feature
 for i=1:size(feat_matrix, 1)
@@ -37,12 +51,19 @@ for i=1:size(feat_matrix, 1)
     if ((sorted_euclidean_dist_loop(1)/sorted_euclidean_dist_loop(2)) > 0.5)
         feat_matrix(i,1) = NaN;
         feat_matrix(i,2) = NaN;
+        xx(i,1) = NaN;
+        xx(i,2) = NaN;
+        yy(i,1) = NaN;
+        yy(i,2) = NaN;
         continue;
     end
     min_ind = find(euclidean_dist_loop == sorted_euclidean_dist_loop(1));
     Tc = cols2(min_ind) - cols1(i) ;
     Tr = rows2(min_ind) - rows1(i) ;
-    
+    xx(i,1) = cols1(i);
+    xx(i,2) = cols2(min_ind);
+    yy(i,1) = rows1(i);
+    yy(i,2) = rows2(min_ind);
     feat_matrix(i,1) = Tr;
     feat_matrix(i,2) = Tc;
 end
@@ -62,14 +83,21 @@ Tc_median = colSortedTrans(floor((firstNaN-1)/2));
 translationMedian = [Tr_median Tc_median];
 
 stitchedImageMedian = stitch(img_right, img_left, translationMedian);
-figure;
+figure(8);
 imshow(stitchedImageMedian, []);
 title('Stitched image with median');
-
-
-
+pause(0.5);
 %%
+nonzeroxx = [xx(xx(:,1) ~= 0,1), xx(xx(:,1) ~= 0,2)];
+notnanxx = [nonzeroxx(~isnan(nonzeroxx(:,1)),1), nonzeroxx(~isnan(nonzeroxx(:,1)),2)] ;
+xx = notnanxx;
 
+nonzeroyy = [yy(yy(:,1) ~= 0,1), yy(yy(:,1) ~= 0,2)];
+notnanyy = [nonzeroyy(~isnan(nonzeroyy(:,1)),1), nonzeroyy(~isnan(nonzeroyy(:,1)),2)] ;
+yy = notnanyy;
+
+N = size(xx,1);
+%%
 % Constructing the System Matrix
 % Next, construct the system matrix S, a 20x9 matrix whose rows are given
 % by [xLxR, xLyR, xL, yLxR, yLyR, yL, xR, yR, 1], where xL,xR and yL,yR are
