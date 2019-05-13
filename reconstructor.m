b843f6d563e4833c89f602539a2fffff9cb9df09
lehung121 <lehung@grinnell.edu>
2019-05-13 14:06:19 -0500

    recontructor commit

--- reconstructor.m
+++ reconstructor.m
@@ -1,2 +1,46 @@
 % 3D Streo Reconstructor 
 
+% function to reconstruct a set of 3D points from their 2-view image
+% projections and the corresponding camera matrices. Using the mid-point
+% method to determine the 3D point as the closest midpoint of the two rays
+% this method is fast but not optimal with respect to the reprojection error
+%
+% inputs: 
+% P0, P1 - 2x4 projection matrices
+% pts0, pts1 - 2xn corresponding image projections
+%
+% output: 
+% pts3D - 3xn set of Euclidean reconstructed 3D points
+
+function [pts3D] = reconstructor(Pl, Pr, ptsl, ptsr)
+  
+  % determine the number of points
+  npts=size(ptsl, 2);
+  
+  
+  A=zeros(3, 2);
+  b=zeros(3, 1);
+  pts3D=zeros(3, npts);
+  
+  Ml=Pl(1:3, 1:3);
+  %inverse matrix M0
+  Ml=inv(Ml);
+  cl=-Ml*Pl(:, 4);
+  Mr=Pr(1:3, 1:3);
+  Mr=inv(Mr);
+  cr=-Mr*Pr(:, 4);
+  for i=1:npts
+    A=[Ml*[ptsl(:,i);1], -Mr*[ptsr(:,i);1]];
+    b=cr-cl;
+    [Q, R]=qr(A); a=R\(Q'*b); % LS with QR
+    pts3D(:,i)=(cl+a(1)*A(:,1) + cr-a(2)*A(:,2))*0.5;
+
+  end
+end
+
+  
+
+
+  
+  
+  
